850faee494a85a9fa616618012c8bafbd8174ee2
Thomas Countz <thomascountz@gmail.com>
2018-10-16 15:20:51 -0400

    add board

---
+++ src/Board.hs
@@ -0,0 +1,58 @@
+module Board (
+  Symbol (..),
+  initBoard,
+  size,
+  result,
+  tie,
+  win
+) where
+
+data Symbol = X | O | Empty deriving (Eq, Show)
+type Board = [Symbol]
+
+initBoard :: Int -> Board
+initBoard size = replicate (size^2) Empty
+
+size :: Board -> Int
+size = round . sqrt . fromIntegral . length
+
+result :: Board -> Int -> Symbol -> Board
+result [] _ _ = []
+result (head:tail) index symbol 
+  | index == 0 = symbol:tail
+  | otherwise = head:result tail (index - 1) symbol
+
+tie :: Board -> Bool
+tie = notElem Empty
+
+win :: Board -> Bool
+win board = any (winAt board) (winningIndices board)
+
+winAt :: Board -> [Int] -> Bool
+winAt board indices = all (== X) (board `atIndices` indices) ||
+                        all (== O) (board `atIndices` indices)
+
+atIndices :: Board -> [Int] -> [Symbol]
+atIndices board = map (\i -> board !! i)
+
+winningIndices :: Board -> [[Int]]
+winningIndices board = winningRowIndices board ++ winningColIndices board ++ winningDiagIndices board
+
+winningRowIndices :: Board -> [[Int]]
+winningRowIndices board = map (\start -> take (size board) [start..]) (rowStarts board)
+
+winningColIndices :: Board -> [[Int]]
+winningColIndices board = map (\start -> take (size board) [start, start + size board..]) (colStarts board)
+
+winningDiagIndices :: Board -> [[Int]]
+winningDiagIndices board = [
+                      take (size board) [0, (size board + 1)..],
+                      take (size board) [size board - 1, ((size board - 1) ^ 2)..]
+                     ]
+
+rowStarts :: Board -> [Int]
+rowStarts board = take (size board) [0, (0 + size board)..]
+
+colStarts :: Board -> [Int]
+colStarts board = take (size board) [0..]
+
---
+++ test/BoardSpec.hs
@@ -0,0 +1,70 @@
+module BoardSpec (spec) where
+import Test.Hspec
+import Board
+
+spec :: Spec
+spec = do
+  describe "initBoard" $ do
+    it "returns a square-length board of a given size" $ do
+      length (initBoard 3) `shouldBe` 9
+      length (initBoard 9) `shouldBe` 81
+
+    it "returns a board of all Empty" $
+      all (==Empty) (initBoard 3) `shouldBe` True
+
+  describe "size" $
+    it "returns the size that a board with initialized with" $ do
+      size (initBoard 3) `shouldBe` 3
+      size (initBoard 9) `shouldBe` 9
+
+  describe "result" $ do
+    context "when the given board is an empty list" $
+      it "returns an empty list" $
+        result [] 0 X `shouldBe` []
+
+    context "when the given index is 0" $
+      it "returns the result of replacing a Symbol at the head of Board" $
+        result [Empty] 0 X `shouldBe` [X]
+  
+
+    context "when the given index is not 0" $
+      it "returns the result of replacing a Symbol at the given index, with the given Symbol" $
+        result [Empty, Empty, Empty] 2 X `shouldBe` [Empty, Empty, X]
+  
+  describe "tie" $ do
+    context "when the board is not complete" $
+      it "returns false" $
+        tie [X, X, O, O, Empty, Empty, Empty, Empty, X] `shouldBe` False
+
+    context "when a player has won" $
+      it "returns false" $
+        tie [X, X, X, Empty, Empty, Empty, Empty, Empty, Empty] `shouldBe` False
+
+    context "when there is a tie game" $
+      it "returns true" $ 
+       tie [X, X, O, O, O, X, X, O, X] `shouldBe` True
+
+  describe "win" $ do
+    context "when the board DOES NOT contain a winner" $
+      it "returns false" $
+       win [X, X, O, O, O, X, X, O, X] `shouldBe` False
+
+    context "when the board DOES contain a winner, X, along a row" $
+      it "returns true" $
+        win [X, X, X, Empty, Empty, Empty, Empty, Empty, Empty] `shouldBe` True 
+
+    context "when the board DOES contain a winner, O, along a row" $
+      it "returns true" $
+        win [O, O, O, Empty, Empty, Empty, Empty, Empty, Empty] `shouldBe` True
+
+    context "when the board DOES contain a winner, along a column" $
+      it "returns true" $
+        win [Empty, X, Empty, Empty, X, Empty, Empty, X, Empty] `shouldBe` True
+
+    context "when the board DOES contain a winner, along a diagonal" $
+      it "returns true" $
+        win [X, Empty, Empty, Empty, X, Empty, Empty, Empty, X] `shouldBe` True
+
+    context "when the board DOES contain a winner, along a diagonal" $
+      it "returns true" $
+        win [Empty, Empty, X, Empty, X, Empty, X, Empty, Empty] `shouldBe` True
