219fa42ee87a5095ef41c80ea89699fcb8a55c30
Thomas Countz <thomascountz@gmail.com>
2018-10-19 09:53:57 -0400

    WIP

--- src/Minimax.hs
+++ src/Minimax.hs
@@ -2,6 +2,28 @@ module Minimax where
 import Board
 import Data.List
 
+minimax' :: Board -> Int
+minimax' board = fst (maximumBy (\(_, a) (_, b) -> compare a b) (map (\move -> (move, minPlay (result board move O))) (availableSpaces board)))
+
+minPlay' :: Board -> Int
+minPlay' board = if terminal board
+                then utility board
+                else minimum (
+                  score = playMoveMax board (head (availableSpaces board))
+                )
+
+playMoveMax :: Board -> Int -> Int
+playMoveMax board position = minPlay (result board position X)
+
+playMoveMin :: Board -> Int -> Int
+playMoveMin board position = maxPlay (result board position O)
+
+maxPlay' :: Board -> Int
+maxPlay' board = if terminal board
+                then utility board
+                else maximum (map (\move -> minPlay (result board move O)) (availableSpaces board))
+
+
 minimax :: Board -> Int
 minimax board = fst (maximumBy (\(_, a) (_, b) -> compare a b) (map (\move -> (move, minPlay (result board move O))) (availableSpaces board)))
 
